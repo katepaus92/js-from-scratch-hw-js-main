7151347d1c52ad498a3c0ecc6ff6479a07f9446a
Katya Paus <katepaus92@gmail.com>
2026-02-19 12:43:48 +0300

    lesson04-01

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -13,4 +13,16 @@
 includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
-function includesElement() {}
+function includesElement(array, element) {
+    let res = true
+    for (let i = 0; i < array.length; i++){        
+        if (array[i] !== element){
+            res = false
+        } else {
+            res = true
+            return res
+        }
+    }
+    return res
+}
+console.log("includesElement: ", includesElement([1, 0, 3], 3))
