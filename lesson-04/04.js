d52eb522e611ee00036d8160f1b1c55bc446ed25
Katya Paus <katepaus92@gmail.com>
2026-02-20 10:03:58 +0300

    lesseon04-4

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -12,4 +12,13 @@
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
 
-function doubleEachCharacter() {}
+function doubleEachCharacter(str) {    
+    let string = Array.from(str)
+    let res = []
+    for (let i=0; i<string.length; i++){
+        res.push(string[i], string[i])        
+    }
+
+    return res.join('')
+}
+console.log("doubleEachCharacter:", doubleEachCharacter('hello'));
